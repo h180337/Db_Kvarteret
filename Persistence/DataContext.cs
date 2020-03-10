235b79393216c001cfb96d563c34d86d49bb96f5
wave4life <superszura@gmail.com>
2020-03-10 16:27:50 +0100

    Starter setup

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //table name inside sqlite
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new {Id = 1, Name = "Value 101"},
+                new {Id = 2, Name = "Value 102"},
+                new {Id = 3, Name = "Value 103"}
+            );
+        }
+    }
+}
