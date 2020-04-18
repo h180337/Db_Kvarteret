58c6fd5b0f5b21632423827a3b4c42212077bb71
wave4life <superszura@gmail.com>
2020-04-18 09:55:51 +0200

    FE: Add tags in search

--- front-end/src/app/common/searchFilter/DataSearch.tsx
+++ front-end/src/app/common/searchFilter/DataSearch.tsx
@@ -1,7 +1,6 @@
-import React, { useContext, useState} from 'react';
+import React, {useState} from 'react';
 import {observer} from 'mobx-react-lite';
-import {RootStoreContext} from "../../stores/rootStore";
-import { Input, Label, Button} from 'semantic-ui-react';
+import {Input, Label} from 'semantic-ui-react';
 import _ from 'lodash';
 import {runInAction} from "mobx";
 
@@ -11,23 +10,28 @@ interface IProps {
 }
 
 const DataSearch: React.FC<IProps> = ({filteredData, dataArray}) => {
-    
-    const [tagSearch, setTagSearch] = useState<string[]>(['test', 'test2', 'test3', 'test4'])
-    
-    const onClickDeleteHandler = (i:number) => {
+
+    const [tagSearch, setTagSearch] = useState<string[]>([])
+
+    const onClickDeleteHandler = (i: number) => {
         setTagSearch([...tagSearch].filter((tag, index) => index !== i))
     }
-    
-    const filter = (e: any, filtered: Map<any,any>, data: any[]) => {
-        let inputdata:string = e.target.value;
+
+    const filter = (e: any, filtered: Map<any, any>, data: any[]) => {
+        let inputdata: string = e.target.value;
         handleFilter(inputdata, data, filtered)
+    }
+
+    const addTags = (event: any) => {
+        setTagSearch([...tagSearch, event.target.value])
+        event.target.value = '';
     }
 
     const handleFilter = _.debounce((inputvalue, data, filtered) => {
         filteredData.clear()
-         data.filter((item: any) => {
-            return Object.keys(item).some((key:string) => {
-                if (item[key] !== null){
+        data.filter((item: any) => {
+            return Object.keys(item).some((key: string) => {
+                if (item[key] !== null) {
                     if (item[key].toString().toLowerCase().includes(inputvalue.toLowerCase())) {
                         runInAction('dataFilter', () => {
                             filteredData.set(item.id, item)
@@ -43,14 +47,14 @@ const DataSearch: React.FC<IProps> = ({filteredData, dataArray}) => {
             <h2>Search</h2>
             {tagSearch.map((tag, index) => (
                 <Label
-                onClick={()=> onClickDeleteHandler(index)}    
-                key={index} 
-                size='large' 
-                color='green'
-                style={{marginBottom: '3px'}}
+                    onClick={() => onClickDeleteHandler(index)}
+                    key={index}
+                    size='large'
+                    color='green'
+                    style={{marginBottom: '3px'}}
                 >{tag}</Label>))}
-            <Input onChange={(e) =>
-                filter(e, filteredData, dataArray)}/>
+
+            <Input onKeyUp={(e: any) => e.key === 'Enter' && addTags(e)}/>
         </div>
     );
 }
