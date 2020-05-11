fc31cdcf4224a4f9e7e5b1cdbcabaea031933ace
h182718 <kjetil.hunshammer2@gmail.com>
2020-05-11 21:36:36 +0200

    SQ Server Production

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,7 +45,7 @@ namespace API
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseLazyLoadingProxies();
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
             ConfigureServices(services);
         }
